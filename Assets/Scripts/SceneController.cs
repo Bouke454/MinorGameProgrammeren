2ec97a29e93ff23ee3db23c0433d9a3e2ce60a3e
DESKTOP-TBI0NLQ\bouke <bouke91@live.nl>
2018-10-23 15:34:33 +0200

    #1 Maken van een menu gerealiseerd
    Verbeteringen:
    -het bord kan aangepast worden op grootte en aantal kaarten per kolom en rij
    -Aantal foutjes aangepakt en verbeterd binnen Unity (cursor error etc)

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
 
-    public const int gridRows = 2;
-    public const int gridCols = 4;
-    public const float offsetX = 4f;
-    public const float offsetY = 5f;
+    //De grootte van het bord
+    public int cardRows = 2;
+    public int cardCols = 4;
+    //Aantal kaarten op het bord 
+    public int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 };
 
+    //Afstand van de kaarten op het bord
+    public float cardPositionX = 4f;
+    public float cardPositionY = 5f;
+
+    //Aantal kogels
     public int ammo = 3;
     [SerializeField] public TextMesh ammoLabel;
     [SerializeField] private TextMesh scoreLabel;
@@ -18,13 +24,12 @@ public class SceneController : MonoBehaviour {
     private MainCard _secondRevealed;
 
     private int _score = 0;
-    private float timer;
-    private bool canCount = false;
-    private bool doOnce = false;
+
 
     public AudioSource Gun;
     public AudioSource Score;
     [SerializeField] private MainCard originalCard;
+    //Aantal verschillende kaarten op het bord
     [SerializeField] private Sprite[] images;
     
 
@@ -32,11 +37,10 @@ public class SceneController : MonoBehaviour {
         ammoLabel.text = "Ammo: " + ammo;
         Vector3 startPos = originalCard.transform.position; //The position of the first card. All other cards are offset from here.
 
-        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
         numbers = ShuffleArray(numbers); //This is a function we will create in a minute!
 
-        for (int i = 0; i < gridCols; i++) {
-            for (int j = 0; j < gridRows; j++) {
+        for (int i = 0; i < cardCols; i++) {
+            for (int j = 0; j < cardRows; j++) {
                 MainCard card;
                 if (i == 0 && j == 0) {
                     card = originalCard;
@@ -44,12 +48,12 @@ public class SceneController : MonoBehaviour {
                     card = Instantiate(originalCard) as MainCard;
                 }
 
-                int index = j * gridCols + i;
+                int index = j * cardCols + i;
                 int id = numbers[index];
                 card.ChangeSprite(id, images[id]);
 
-                float posX = (offsetX * i) + startPos.x;
-                float posY = (offsetY * j) + startPos.y;
+                float posX = (cardPositionX * i) + startPos.x;
+                float posY = (cardPositionY * j) + startPos.y;
                 card.transform.position = new Vector3(posX, posY, startPos.z);
             }
         }
@@ -74,7 +78,6 @@ public class SceneController : MonoBehaviour {
     }
 
     public void CardRevealed(MainCard card) {
-        Debug.Log(ammo);
         if (_firstRevealed == null && ammo != 0) {
             _firstRevealed = card;
             Gun.Play();
@@ -93,7 +96,12 @@ public class SceneController : MonoBehaviour {
         if (_firstRevealed.id == _secondRevealed.id) {
             Score.Play();
             _score++;
-            scoreLabel.text = "Score: " + _score;
+            scoreLabel.text = "Combo: " + _score;
+            //bekijken of de score gelijk is aan het aantal kaarten omgeslagen per level
+            var cardsLeft = cardCols * cardRows / 2;
+            if (_score == cardsLeft) {
+                Debug.Log("einde van het level");
+            }
         } else {
             yield return new WaitForSeconds(0.5f);
 
@@ -107,6 +115,6 @@ public class SceneController : MonoBehaviour {
     }
 
     public void Restart() {
-        SceneManager.LoadScene("Scene_01");
+        SceneManager.LoadScene("Menu");
     }
 }
